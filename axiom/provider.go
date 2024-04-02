d5e7dfcd385494cd0ed4a9691a9674c8f12c69df
Topper <topper@topper.dev>
2024-04-02 18:38:24 +0100

    bump version

--- axiom/provider.go
+++ axiom/provider.go
@@ -15,7 +15,7 @@ import (
 )
 
 const (
-	providerUserAgent = "terraform-provider-axiom/v0.0.1"
+	providerUserAgent = "terraform-provider-axiom/v1.0.1"
 )
 
 // Ensure the implementation satisfies the expected interfaces
